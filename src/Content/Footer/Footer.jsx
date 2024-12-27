26dbee450f56d9226fab6dfad9fe2b15982e0f86
Soumil Biswas <soumil.biswas19@gmail.com>
2024-12-27 16:39:49 +0530

    Deployment commit

--- src/Content/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return(
-        <section className="flex flex-col items-center w-full bg-[--highlight-color]">
-             <p className="py-10">Copyright© 2024. All Rights Reserved.</p>
-        </section>
-    )
-}
